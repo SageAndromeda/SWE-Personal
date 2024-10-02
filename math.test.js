f168a4d06e471f1785e73966aca35dab1b22744b
Sage Andromeda H. <143134634+SageAndromeda@users.noreply.github.com>
2024-10-02 23:53:28 +0000

    workflow

---
+++ math.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert')
+
+
+// Test the addNumbers funtion
+describe('addNumbers', function ( {
+    it('should return 15 when adding 5 and 10', function(){
+        const result = math.addNumbers(5,10);
+        assert(strictEqual(result,15));
+    })
+});
